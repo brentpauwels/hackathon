0d777d255f78d6164d358f7f5fe684c55e1ff76b
Toon Van Havermaet <toon.vanhavermaet@student.odisee.be>
2022-06-08 19:04:07 +0200

    add code for hallsensor, first draft master/slave

--- joystick_with_8x8matrix/joystick_with_8x8matrix.ino
+++ joystick_with_8x8matrix/joystick_with_8x8matrix.ino
@@ -1,8 +1,12 @@
 #include "LedControl.h"
 //source LedControl: http://wayoda.github.io/LedControl/
 //source joystick: https://arduinogetstarted.com/tutorials/arduino-joystick
+#include <Wire.h>
 
-int code = 0; //code door moeder Arduino
+const int arduinoID = 11;         // MOET UNIQUE ZIJN T.O.V ANDERE ARDUINOS !!!
+int code = -1;
+int solved = 1;                  // 1 == solved, 0 != solved
+
 switch (code) {
 default:
   int Heart[8][8] {
@@ -147,65 +151,89 @@ int yValue = 0; // yCo storage default for y is 521, full up = 0, down = 1023
 unsigned long delaytime = 1000; //startdelay
 
 void setup() {
-  Serial.begin(9600); //for debugging: joystick reads x and y as analogue values
   lc.shutdown(0, false); //wakes up the 7219 IC (default is off for power saving)
   lc.setIntensity(0, 15); //set leds intensity between 0 and 15
   lc.clearDisplay(0); // turn off entire display
+
+  Wire.begin(arduinoID);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+  Wire.onRequest(puzzleSolved);
+  Serial.begin(115200);           // start serial for output
 }
 
-void matrix() {
-  xValue = analogRead(X_PIN); //get initial xValue
-  yValue = analogRead(Y_PIN); //get intial yValue
-  if (yValue > 521) { //joystick is pointing down
-    for (int row = 7; row >= 0; row--) { //iterate over 8 rows en 8 cols from right down to left up
-      for (int col = 7; col >= 0; col--) {
-        int yValue2 = analogRead(Y_PIN);
-        if ( yValue2 < yValue - 10 || yValue2 > yValue + 10 ) { //check if yValue changed, margin for hysteresis
-          yValue = yValue2;
-          break;
-        }
-        xValue = analogRead(X_PIN);
-        if (xValue > 700 && delaytime > 100 ) { //joystick pointing right, margin for hysteresis
-          delaytime -= 100;
-        }
-        else if (xValue < 300 && delaytime <= 1000) { //joystick pointing left, margin for hysteresis
-          delaytime += 100;
-        }
-        if (Four[row][col] == 1) { //print the right led
-          lc.setLed(0, row, col, true); //setLed method: 0 = address (only for multiple displays), row, col, true/1 = led on and false/0 = led off
-          delay(delaytime);
-          lc.clearDisplay(0);
-        }
-      }
-    }
-  }
-
-  //case joystick mid or pointing up (DEFAULT)
-  else {
-    for (int row = 0; row < 8; row++) { //iterate over 8 rows en 8 cols from left up to right down
-      for (int col = 0; col < 8; col++) {
-        delay(delaytime);
-        int yValue2 = analogRead(Y_PIN);
-        if (yValue2 < yValue - 10 || yValue2 > yValue + 10 ) { //check if yValue changed, margin for hysteresis
-          yValue = yValue2;
-          break;
-        }
-        xValue = analogRead(X_PIN);
-        if (xValue > 700 && delaytime > 100 ) { //joystick pointing right, room for hysteresis
-          delaytime -= 100;
-        }
-        else if (xValue < 300 && delaytime <= 200) { //joystick pointing left, margin for hysteresis
-          delaytime += 100;
-        }
-        if (Four[row][col] == 1) { //print getal
-          lc.setLed(0, row, col, true); //setLed method: 0 = address (only for multiple displays), row, col, true/1 = led on and false/0 = led off
-          delay(delaytime);
-          lc.clearDisplay(0);
-        }
-      }
-    }
-  }
-}
 void loop() {
-  matrix();
+  if (code >= 0) { // do, als code ontvangen is (dus boven 0 ligt)
+    if (solved == 1) {
+      solved = 0;
+    }
+
+    xValue = analogRead(X_PIN); //get initial xValue
+    yValue = analogRead(Y_PIN); //get intial yValue
+    if (yValue > 521) { //joystick is pointing down
+      for (int row = 7; row >= 0; row--) { //iterate over 8 rows en 8 cols from right down to left up
+        for (int col = 7; col >= 0; col--) {
+          int yValue2 = analogRead(Y_PIN);
+          if ( yValue2 < yValue - 10 || yValue2 > yValue + 10 ) { //check if yValue changed, margin for hysteresis
+            yValue = yValue2;
+            break;
+          }
+          xValue = analogRead(X_PIN);
+          if (xValue > 700 && delaytime > 100 ) { //joystick pointing right, margin for hysteresis
+            delaytime -= 100;
+          }
+          else if (xValue < 300 && delaytime <= 1000) { //joystick pointing left, margin for hysteresis
+            delaytime += 100;
+          }
+          if (Four[row][col] == 1) { //print the right led
+            lc.setLed(0, row, col, true); //setLed method: 0 = address (only for multiple displays), row, col, true/1 = led on and false/0 = led off
+            delay(delaytime);
+            lc.clearDisplay(0);
+          }
+        }
+      }
+    }
+
+    //case joystick mid or pointing up (DEFAULT)
+    else {
+      for (int row = 0; row < 8; row++) { //iterate over 8 rows en 8 cols from left up to right down
+        for (int col = 0; col < 8; col++) {
+          delay(delaytime);
+          int yValue2 = analogRead(Y_PIN);
+          if (yValue2 < yValue - 10 || yValue2 > yValue + 10 ) { //check if yValue changed, margin for hysteresis
+            yValue = yValue2;
+            break;
+          }
+          xValue = analogRead(X_PIN);
+          if (xValue > 700 && delaytime > 100 ) { //joystick pointing right, room for hysteresis
+            delaytime -= 100;
+          }
+          else if (xValue < 300 && delaytime <= 200) { //joystick pointing left, margin for hysteresis
+            delaytime += 100;
+          }
+          if (Four[row][col] == 1) { //print getal
+            lc.setLed(0, row, col, true); //setLed method: 0 = address (only for multiple displays), row, col, true/1 = led on and false/0 = led off
+            delay(delaytime);
+            lc.clearDisplay(0);
+          }
+        }
+      }
+    }
+    delay(1000);
+  }
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  while (1 < Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  code = Wire.read();    // receive byte as an integer
+  Serial.println(code);         // print the integer
+ 
+}
+
+void puzzleSolved() {
+    Wire.write(solved);
 }
---
+++ nummerplaat/nummerplaat.ino
@@ -0,0 +1,90 @@
+//source 7segment code: https://create.arduino.cc/projecthub/aboda243/get-started-with-seven-segment-c73200
+#include <Wire.h>
+const int arduinoID = 10;         // MOET UNIQUE ZIJN T.O.V ANDERE ARDUINOS !!!
+int code = -1;
+int solved = 1;                  // 1 == solved, 0 != solved
+char c = -1;
+
+byte Digits[10][8] {
+  {'0', 1, 1, 1, 1, 1, 1, 0}, //0
+  {'1', 0, 1, 1, 0, 0, 0, 0}, //1
+  {'2', 1, 1, 0, 1, 1, 0, 1}, //2
+  {'3', 1, 1, 1, 1, 0, 0, 1}, //3
+  {'4', 0, 1, 1, 0, 0, 1, 1}, //4
+  {'5', 1, 0, 1, 1, 0, 1, 1}, //5
+  {'6', 1, 0, 1, 1, 1, 1, 1}, //6
+  {'7', 1, 1, 1, 0, 0, 0, 0}, //7
+  {'8', 1, 1, 1, 1, 1, 1, 1}, //8
+  {'9', 1, 1, 1, 1, 0, 1, 1}, //9
+};
+
+#define A 12
+#define B 11
+#define C 10
+#define D 9
+#define E 8
+#define F 7
+#define G 6
+int seg[] {A, B, C, D, E, F, G};        //note: no use for Decimal Point, so it's omitted
+const int Q = 13;
+int sensorState = 1;                    //sensor starts 1
+void setup() {
+  Wire.begin(arduinoID);                // join i2c bus with address #10
+  Wire.onReceive(receiveEvent);         // register event
+  Wire.onRequest(puzzleSolved);
+  Serial.begin(115200);                 // start serial for output
+
+  pinMode(seg[0], OUTPUT);              //all pins of the 7 segment are outputs
+  pinMode(seg[1], OUTPUT);
+  pinMode(seg[2], OUTPUT);
+  pinMode(seg[3], OUTPUT);
+  pinMode(seg[4], OUTPUT);
+  pinMode(seg[5], OUTPUT);
+  pinMode(seg[6], OUTPUT);
+  pinMode(Q, INPUT);                 //button as input
+}
+
+void loop() {
+
+  if (code >= 0) { // do, als code ontvangen is (dus boven 0 ligt)
+    digitalRead(sensorState);
+    if (digitalRead(sensorState) == LOW) {                                 //sensor has read a magnet
+      unsigned long magnet_read = millis();                             //unsigned because always >= 0
+      if (millis() == magnet_read + 1000 && digitalRead(Q) == LOW) { //make sure the sensor sees the magnet for at least 1 whole second before showing answer
+        Print(c);
+        solved = 1;
+      }
+    }
+  }
+  delay(1000);
+  if (solved == 1) {
+    solved = 0;
+  }
+}
+
+void Print(char Char) {  // function for printing char
+  int index = -1;     // set search result to -1
+  for (int i = 0; i < 10 ; i++) { //search for the entered character
+    if (Char == Digits[i][0]) { //if the character found
+      index = i;//set the result number into index
+    }
+  }
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(seg[i], Digits[index][i + 1]);
+  }
+}
+
+void receiveEvent(int howMany) {
+  while (1 < Wire.available()) {  // loop through all but the last
+    char c = Wire.read();         // receive byte as a character
+    Serial.print(c);              // print the character
+  }
+
+  code = Wire.read();            // receive byte as an integer
+  Serial.println(code);         // print the integer
+
+}
+
+void puzzleSolved() {
+  Wire.write(solved);
+}
