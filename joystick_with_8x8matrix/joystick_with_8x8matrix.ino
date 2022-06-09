54b5367365cab4c895719fd816d697d5e1b2d6dd
Toon Van Havermaet <toon.vanhavermaet@student.odisee.be>
2022-06-09 11:40:09 +0200

    most recent ledmatrix code

--- joystick_with_8x8matrix/joystick_with_8x8matrix.ino
+++ joystick_with_8x8matrix/joystick_with_8x8matrix.ino
@@ -6,149 +6,14 @@
 const int arduinoID = 11;         // MOET UNIQUE ZIJN T.O.V ANDERE ARDUINOS !!!
 int code = -1;
 int solved = 1;                  // 1 == solved, 0 != solved
-
-switch (code) {
-default:
-  int Heart[8][8] {
-    {0, 1, 1, 0, 0, 1, 1, 0},
-    {1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1},
-    {0, 1, 1, 1, 1, 1, 1, 0},
-    {0, 0, 1, 1, 1, 1, 0, 0},
-    {0, 0, 0, 1, 1, 0, 0, 0}
-  };
-  break;
-case 1:
-  int One[8][8] = {
-    {0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 1, 1, 0, 0, 0, 0},
-    {0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 1, 1, 1, 0, 0, 0}
-  };
-  break;
-case 2:
-  int Two[8][8] = {
-    {0, 0, 1, 1, 1, 0, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 1, 0, 0, 0},
-    {0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 1, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 1, 1, 0, 0}
-  };
-  break;
-case 3:
-  int Three[8][8] = {
-    {0, 0, 1, 1, 1, 0, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 1, 1, 0, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 0, 1, 1, 1, 0, 0, 0}
-  };
-  break;
-case 4:
-  int Four[8][8] =  {
-    {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 1, 0, 0},
-    {0, 0, 0, 1, 0, 1, 0, 0},
-    {0, 0, 1, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 1, 1, 1, 1, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0}
-  };
-  break;
-case 5:
-  int Five[8][8] = {
-    {0, 1, 1, 1, 1, 1, 0, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 1, 0, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 0, 1, 1, 1, 0, 0, 0}
-  };
-  break;
-case 6:
-  int Six[8][8] = {
-    {0, 0, 1, 1, 1, 0, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 1, 0, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 0, 1, 1, 1, 0, 0, 0}
-  };
-  break;
-case 7:
-  int Seven[8][8] = {
-    {0, 1, 1, 1, 1, 1, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 1, 0, 0, 0},
-    {0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 1, 0, 0, 0, 0, 0},
-    {0, 0, 1, 0, 0, 0, 0, 0},
-    {0, 0, 1, 0, 0, 0, 0, 0}
-  };
-  break;
-case 8:
-  int Eight[8][8] = {
-    {0, 0, 1, 1, 1, 0, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 0, 1, 1, 1, 0, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 0, 1, 1, 1, 0, 0, 0}
-  };
-  break;
-case 9:
-  int Nine[8][8] = {
-    {0, 0, 1, 1, 1, 0, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 0, 1, 1, 1, 1, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 0, 1, 1, 1, 0, 0, 0}
-  };
-  break;
-case 0:
-  int Zero[8][8] = {
-    {0, 0, 1, 1, 1, 0, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 0, 1, 1, 1, 0, 0, 0}
-  };
-  break;
-}
-
+int Digit[8][8];
 LedControl lc = LedControl(12, 11, 10, 1); //12 = DataIn, 11 = CLK, 10 = LOAD
 
-#define X_PIN  A0 // joystick X_PIN analogue pin
-#define Y_PIN  A1 // joystick Y_PIN analogue pin 
+#define X_PIN  A0 // joystick X_PIN analog pin
+#define Y_PIN  A1 // joystick Y_PIN analog pin 
 int xValue = 0; // xCo storage, default for x is  513, full left = 0, right = 1023
 int yValue = 0; // yCo storage default for y is 521, full up = 0, down = 1023
-unsigned long delaytime = 1000; //startdelay
+unsigned long delaytime = 1010; //startdelay
 
 void setup() {
   lc.shutdown(0, false); //wakes up the 7219 IC (default is off for power saving)
@@ -168,6 +33,9 @@ void loop() {
     if (yValue > 521) { //joystick is pointing down
       for (int row = 7; row >= 0; row--) { //iterate over 8 rows en 8 cols from right down to left up
         for (int col = 7; col >= 0; col--) {
+          lc.setLed(0, row, col, Digit[row][col]); //setLed method: 0 = address (only for multiple displays), row, col, true/1 = led on and false/0 = led off
+          delay(delaytime);
+          lc.clearDisplay(0);
           int yValue2 = analogRead(Y_PIN);
           if ( yValue2 < yValue - 10 || yValue2 > yValue + 10 ) { //check if yValue changed, margin for hysteresis
             yValue = yValue2;
@@ -180,11 +48,6 @@ void loop() {
           else if (xValue < 300 && delaytime <= 1000) { //joystick pointing left, margin for hysteresis
             delaytime += 100;
           }
-          if (Four[row][col] == 1) { //print the right led
-            lc.setLed(0, row, col, true); //setLed method: 0 = address (only for multiple displays), row, col, true/1 = led on and false/0 = led off
-            delay(delaytime);
-            lc.clearDisplay(0);
-          }
         }
       }
     }
@@ -193,7 +56,9 @@ void loop() {
     else {
       for (int row = 0; row < 8; row++) { //iterate over 8 rows en 8 cols from left up to right down
         for (int col = 0; col < 8; col++) {
+          lc.setLed(0, row, col, true); //setLed method: 0 = address (only for multiple displays), row, col, true/1 = led on and false/0 = led off
           delay(delaytime);
+          lc.clearDisplay(0);
           int yValue2 = analogRead(Y_PIN);
           if (yValue2 < yValue - 10 || yValue2 > yValue + 10 ) { //check if yValue changed, margin for hysteresis
             yValue = yValue2;
@@ -205,11 +70,6 @@ void loop() {
           }
           else if (xValue < 300 && delaytime <= 200) { //joystick pointing left, margin for hysteresis
             delaytime += 100;
-          }
-          if (Four[row][col] == 1) { //print getal
-            lc.setLed(0, row, col, true); //setLed method: 0 = address (only for multiple displays), row, col, true/1 = led on and false/0 = led off
-            delay(delaytime);
-            lc.clearDisplay(0);
           }
         }
       }
@@ -226,9 +86,153 @@ void receiveEvent(int howMany) {
   }
   code = Wire.read();    // receive byte as an integer
   Serial.println(code);         // print the integer
- 
+switch (code) {
+default:
+  Digit[8][8] = {
+    {0, 1, 1, 0, 0, 1, 1, 0},
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 1, 1, 1, 1, 1, 1, 0},
+    {0, 0, 1, 1, 1, 1, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0}
+  };
+  break;
+
+case 1:
+  Digit[8][8] = {
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 1, 1, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0}
+  };
+  break;
+
+case 2:
+  Digit[8][8] = {
+    {0, 0, 1, 1, 1, 0, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 1, 1, 1, 1, 1, 0, 0}
+  };
+  break;
+
+case 3:
+  Digit[8][8] = {
+    {0, 0, 1, 1, 1, 0, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0}
+  };
+  break;
+
+case 4:
+  Digit[8][8] =  {
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 1, 0, 0},
+    {0, 0, 0, 1, 0, 1, 0, 0},
+    {0, 0, 1, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 1, 1, 1, 1, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0}
+  };
+  break;
+
+case 5:
+  Digit[8][8] = {
+    {0, 1, 1, 1, 1, 1, 0, 0},
+    {0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 1, 1, 1, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0}
+  };
+  break;
+
+case 6:
+  Digit[8][8] = {
+    {0, 0, 1, 1, 1, 0, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 1, 1, 1, 1, 0, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0}
+  };
+  break;
+
+case 7:
+  Digit[8][8] = {
+    {0, 1, 1, 1, 1, 1, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0}
+  };
+  break;
+
+case 8:
+  Digit[8][8] = {
+    {0, 0, 1, 1, 1, 0, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0}
+  };
+  break;
+
+case 9:
+  Digit[8][8] = {
+    {0, 0, 1, 1, 1, 0, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 0, 1, 1, 1, 1, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0}
+  };
+  break;
+
+case 0:
+  Digit[8][8] = {
+    {0, 0, 1, 1, 1, 0, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0}
+  };
+  break;
+
+}
 }
 
 void puzzleSolved() {
-    Wire.write(solved);
+  Wire.write(solved);
 }
